68d3bbcdd19d596b8efd16cc6e47895576e3b240
Keerthy Shunmugarajan <keerthy.shunmugarajan@yahoo.com>
2025-10-24 21:21:02 +0530

    setting up prod env variables

--- FrontEnd_/custom_feemanagement/src/views/App/index.tsx
+++ FrontEnd_/custom_feemanagement/src/views/App/index.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import TestView from "../TestView";
 import StudentRegistrationForm from "../StudentRegistrationForm";
 import LoginForm from "../LoginForm";
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import HeaderTab from "../HeaderTab";
 import StudentDetailsForm from "../StudentDetailsForm";
 
 const App = () => {
+  console.log("NODE_ENV:", process.env.NODE_ENV);
+  console.log("BASE_URL:", process.env.BASE_URL);
   const message = "Hello Frontend_Child"
   return (
 
     <Routes>
-      <Route path="/" element={<Navigate to='/login'></Navigate>} />
+      <Route path="/" element={<LoginForm />} />
+
+      {/* <Route path="/" element={<Navigate to='/login'></Navigate>} /> */}
       <Route path="/login" element={<LoginForm />} />
       <Route path="/testPage" element={<TestView message_Parent={message} />} />
       <Route path="/headerTab" element={<HeaderTab />} />
