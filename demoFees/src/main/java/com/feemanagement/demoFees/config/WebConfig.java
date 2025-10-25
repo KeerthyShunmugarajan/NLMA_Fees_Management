b85c89327bc3e0fe3844ab7e17732230096b6d2d
Keerthy Shunmugarajan <keerthy.shunmugarajan@yahoo.com>
2025-10-25 23:35:26 +0530

    docker update and webconfig

--- demoFees/src/main/java/com/feemanagement/demoFees/config/WebConfig.java
+++ demoFees/src/main/java/com/feemanagement/demoFees/config/WebConfig.java
@@ -1,4 +1,18 @@
 package com.feemanagement.demoFees.config;
 
-public class WebConfig {
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")  // all API endpoints
+                .allowedOrigins("http://nlma-fee-front.vercel.app/")  // replace with your Vercel URL
+                .allowedMethods("*"); // allow GET, POST, PUT, DELETE, etc.
+    }
 }
