4d4cf7a6e796b3f17ddefc74eed21501b27c4200
Keerthy Shunmugarajan <keerthy.shunmugarajan@yahoo.com>
2025-10-27 12:31:14 +0530

    added port

--- demoFees/src/main/java/com/feemanagement/demoFees/config/WebConfig.java
+++ demoFees/src/main/java/com/feemanagement/demoFees/config/WebConfig.java
@@ -11,6 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
+        //String frontendUrl = System.getenv("FRONTEND_URL");
         registry.addMapping("/**")  // all API endpoints
                 .allowedOrigins("http://nlma-fee-front.vercel.app/")  // replace with your Vercel URL
                 .allowedMethods("*")// allow GET, POST, PUT, DELETE, etc.
